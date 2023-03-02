4ac5e63ecf1343d9682d32a1f44e2c485e5f1d2f
sayed <sayedali.7094>
2023-03-02 13:25:05 +0200

    fourth commit

--- next.config.js
+++ next.config.js
@@ -1,7 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  // staticPageGenerationTimeout: 1000,
+  images: {
+    domains: ["res.cloudinary.com"],
+  },
 };
 
 module.exports = nextConfig;
